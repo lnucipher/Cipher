b35cdc2ab175356d15d1a053028538d98c280a25
Maksym <max.popaylo@gmail.com>
2024-10-01 14:54:40 +0300

    CP-7 Add Auth Screen background

--- Mobile.Client/app/src/main/java/com/example/cipher/ui/screens/auth_screen/AuthScreen.kt
+++ Mobile.Client/app/src/main/java/com/example/cipher/ui/screens/auth_screen/AuthScreen.kt
@@ -1,15 +1,49 @@
 package com.example.cipher.ui.screens.auth_screen
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.cipher.R
 
 
 @Composable
 fun AuthScreen(
     viewModel: AuthScreenViewModel = hiltViewModel()
 ) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF17212B)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxSize(0.35f),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(R.drawable.cipher_logo),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth(0.3f)
+            )
+        }
+    }
+
 }
 
 
