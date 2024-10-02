a616ea67a0d073f8ae28837aab1644f0ba0e68d8
Maksym <max.popaylo@gmail.com>
2024-10-02 19:53:46 +0300

    feat (registration): add Ime state for keyboard animation in AuthScreen

---
+++ Mobile.Client/app/src/main/java/com/example/cipher/ui/screens/auth_screen/composable/ImeState.kt
@@ -0,0 +1,32 @@
+package com.example.cipher.ui.screens.auth_screen.composable
+
+import android.view.ViewTreeObserver
+import android.view.WindowInsets
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+@Composable
+fun rememberImeState(): State<Boolean> {
+    val imeState = remember { mutableStateOf(false) }
+    val view = LocalView.current
+
+    DisposableEffect(view) {
+        val listener = ViewTreeObserver.OnGlobalLayoutListener {
+            val isKeyboardOpen = ViewCompat.getRootWindowInsets(view)
+                ?.isVisible(WindowInsetsCompat.Type.ime()) ?: true
+            imeState.value = isKeyboardOpen
+        }
+        view.viewTreeObserver.addOnGlobalLayoutListener(listener)
+        onDispose {
+            view.viewTreeObserver.removeOnGlobalLayoutListener(listener)
+        }
+    }
+
+    return imeState
+}
