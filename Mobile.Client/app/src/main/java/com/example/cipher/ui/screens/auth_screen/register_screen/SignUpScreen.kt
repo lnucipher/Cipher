cbf2931359a278faf3e6bc7066898d89ded76ff4
Maksym <max.popaylo@gmail.com>
2024-10-03 15:22:03 +0300

    feat (registration): Add SignUpScreen

---
+++ Mobile.Client/app/src/main/java/com/example/cipher/ui/screens/auth_screen/register_screen/SignUpScreen.kt
@@ -0,0 +1,128 @@
+package com.example.cipher.ui.screens.auth_screen.register_screen
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.example.cipher.ui.screens.auth_screen.composable.AuthTextField
+import com.example.cipher.ui.theme.CipherTheme.colors
+import com.example.cipher.ui.theme.CipherTheme.shapes
+import com.example.cipher.ui.theme.CipherTheme.typography
+
+@Composable
+fun SignUpScreen(
+    maxUpperSectionRatio: MutableState<Float>,
+    navigateToAdditionalInfo: () -> Unit,
+    navigateBack:() -> Unit
+) {
+    maxUpperSectionRatio.value = 0.25f
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .clip(RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp))
+            .background(colors.secondaryBackground)
+            .padding(48.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Spacer(modifier = Modifier.fillMaxSize(0.05f))
+        Text(
+            text = "Sign Up",
+            color = colors.primaryText,
+            style = typography.heading
+        )
+        Spacer(modifier = Modifier.fillMaxSize(0.1f))
+
+        AuthTextField(
+            modifier = Modifier
+                .padding(bottom = 16.dp),
+            label = "Login",
+            height = 42.dp,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            keyboardActions = KeyboardActions()
+        ) {
+
+        }
+
+        AuthTextField(
+            modifier = Modifier
+                .padding(bottom = 16.dp),
+            label = "Password",
+            isPassword = true,
+            height = 42.dp,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            keyboardActions = KeyboardActions()
+        ) {
+
+        }
+
+        AuthTextField(
+            modifier = Modifier
+                .padding(bottom = 32.dp),
+            label = "Confirm Password",
+            isPassword = true,
+            height = 42.dp,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            keyboardActions = KeyboardActions()
+        ) {
+
+        }
+
+        Button(
+            onClick = { navigateToAdditionalInfo() },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp).padding(bottom = 12.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = colors.primaryText,
+                containerColor = colors.tintColor
+            ),
+            shape = shapes.componentShape
+        ) {
+            Text(
+                text = "Next",
+                style = typography.toolbar
+            )
+        }
+
+        Button(
+            onClick = { navigateBack() },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = colors.primaryText,
+                containerColor = Color.Transparent
+            ),
+            shape = shapes.componentShape,
+            border = BorderStroke(2.dp, colors.tintColor)
+
+        ) {
+            Text(
+                text = "Cancel",
+                style = typography.toolbar
+            )
+        }
+
+    }
+}
+
+
