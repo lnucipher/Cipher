c0cdf8d2b1b7bf182c9721fe88e17cf6358defeb
wovkun <volpastern@gmail.com>
2024-10-05 02:07:00 +0300

    [CP-6] feat: HTML and CSS for registration screen

--- Web.Client/src/app/app.component.ts
+++ Web.Client/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SignInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Cipher';
 }
