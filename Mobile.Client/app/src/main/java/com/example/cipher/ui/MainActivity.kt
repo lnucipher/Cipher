90956d0d73cbba814466c81656e3f3641689aca8
Maksym <max.popaylo@gmail.com>
2024-10-05 19:56:10 +0300

    [CP-7] fear (registration): Add navigation to MainActivity

--- Mobile.Client/app/src/main/java/com/example/cipher/ui/MainActivity.kt
+++ Mobile.Client/app/src/main/java/com/example/cipher/ui/MainActivity.kt
@@ -4,24 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionScope
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.example.cipher.ui.screens.auth_screen.AuthScreen
 import com.example.cipher.ui.theme.CipherTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalSharedTransitionApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CipherTheme (
-                darkTheme = true
+                darkTheme = false
             ) {
-
+                val navController: NavHostController = rememberNavController()
+                SharedTransitionScope {
+                    NavHost(
+                        navController = navController,
+                        startDestination = NavRoutes.AuthRoute.name
+                    ) {
+                        composable(NavRoutes.AuthRoute.name) {
+                            AuthScreen()
+                        }
+                    }
+                }
             }
         }
     }
 }
 
+enum class NavRoutes {
+    AuthRoute
+}
