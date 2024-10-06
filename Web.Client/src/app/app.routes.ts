290c65b6ab3fee93e920e3f9ffc6a445390dc981
wovkun <volpastern@gmail.com>
2024-10-07 01:58:08 +0300

    [CP-6] feat: created signup,copied code from signin

--- Web.Client/src/app/app.routes.ts
+++ Web.Client/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
     { path: '', component: SignInComponent },
+    { path: 'sign-in', component: SignInComponent },
+    { path: 'sign-up', component: SignUpComponent }
 ];
