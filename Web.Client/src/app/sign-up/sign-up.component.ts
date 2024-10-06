75e81819f731ec9260b7109c75ee7a03a902b245
wovkun <volpastern@gmail.com>
2024-10-07 01:58:08 +0300

    [CP-6] feat: created signup,copied code from signin

---
+++ Web.Client/src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector: 'app-sign-up',
+standalone: true,
+templateUrl:'./sign-up.component.html',
+styleUrl: './sign-up.component.css'
+})
+
+export class SignUpComponent {}
