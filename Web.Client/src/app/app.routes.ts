dbd8c2861738829b8e74efab3a232047c061acdf
wovkun <volpastern@gmail.com>
2024-10-07 17:34:20 +0300

    [CP-6] feat: routing for first-time profile setup

--- Web.Client/src/app/app.routes.ts
+++ Web.Client/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ProfileSetupComponent } from './profile-setup/profile-setup.component';
 
 export const routes: Routes = [
     { path: '', component: SignInComponent },
     { path: 'sign-up', component: SignUpComponent },
     { path: 'sign-in', component: SignInComponent },
+    { path: 'profile-setup', component: ProfileSetupComponent },
 ];
