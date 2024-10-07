11c5025f58b9910330b06caaed8d7e3cfe4f460e
wovkun <volpastern@gmail.com>
2024-10-07 03:03:11 +0300

    [CP-6] feat!: created routing between pages

--- Web.Client/src/app/sign-in/sign-in.component.ts
+++ Web.Client/src/app/sign-in/sign-in.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
 selector: 'app-sign-in',
 standalone: true,
 templateUrl:'./sign-in.component.html',
-styleUrl: './sign-in.component.css'
+styleUrl: './sign-in.component.css',
+imports: [RouterLink]
 })
 
 export class SignInComponent {}
