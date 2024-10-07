86a99b13e50c82ed274387210c317d0b4f4d72af
wovkun <volpastern@gmail.com>
2024-10-07 03:03:11 +0300

    [CP-6] feat!: created routing between pages

--- Web.Client/src/app/app.component.ts
+++ Web.Client/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SignInComponent],
+  imports: [RouterOutlet,SignInComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Web.Client/src/app/app.routes.ts
+++ Web.Client/src/app/app.routes.ts
@@ -1,9 +1,10 @@
-import { Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
     { path: '', component: SignInComponent },
+    { path: 'sign-up', component: SignUpComponent },
     { path: 'sign-in', component: SignInComponent },
-    { path: 'sign-up', component: SignUpComponent }
 ];
