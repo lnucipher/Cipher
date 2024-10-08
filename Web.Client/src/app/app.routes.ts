1c139371761e9d67cc6129395e4eefd1b96c4872
wovkun <volpastern@gmail.com>
2024-10-08 03:11:30 +0300

    [CP-6] docs: fundamental architecture change

--- Web.Client/src/app/app.routes.ts
+++ Web.Client/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { ProfileSetupComponent } from './profile-setup/profile-setup.component';
+import { SignInComponent } from './features/sign-in/sign-in.component';
+import { SignUpComponent } from './features/sign-up/sign-up.component';
+import { ProfileSetupComponent } from './features/profile-setup/profile-setup.component';
 
 export const routes: Routes = [
     { path: '', component: SignInComponent },
