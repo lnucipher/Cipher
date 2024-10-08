4d1e86cd3760c02ec7873970302d20952eadd4cb
wovkun <volpastern@gmail.com>
2024-10-08 03:11:30 +0300

    [CP-6] docs: fundamental architecture change

--- Web.Client/src/app/app.component.ts
+++ Web.Client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet } from '@angular/router';
-import { SignInComponent } from './sign-in/sign-in.component';
+import { SignInComponent } from './features/sign-in/sign-in.component';
 
 @Component({
   selector: 'app-root',
