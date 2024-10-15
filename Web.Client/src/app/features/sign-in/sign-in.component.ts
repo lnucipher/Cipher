129ce44e1a8de03fe8d7db9e4660b372a5f75e6c
wovkun <volpastern@gmail.com>
2024-10-15 03:04:19 +0300

    [CP-40] fe!n: signIn validation

--- Web.Client/src/app/features/sign-in/sign-in.component.ts
+++ Web.Client/src/app/features/sign-in/sign-in.component.ts
@@ -1,11 +1,60 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+  OnInit,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { passwordRegex } from '../../core/validators/regex';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
 })
-export class SignInComponent {}
+export class SignInComponent implements OnInit {
+  public signInForm!: FormGroup;
+  formSubmitted = false;
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  private initializeForm(): void {
+    this.signInForm = new FormGroup({
+      username: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(5),
+        ],
+        updateOn: 'change',
+      }),
+      password: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.pattern(passwordRegex),
+        ],
+        updateOn: 'change',
+      }),
+    });
+  }
+
+  onSubmit(): void {
+    this.formSubmitted = true;
+    if (this.signInForm.valid) {
+      console.log('Form Submitted', this.signInForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
