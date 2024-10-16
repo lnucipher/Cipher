1b90c276e50709235541e38081cf1a59a4c65778
wovkun <volpastern@gmail.com>
2024-10-17 02:18:25 +0300

    [CP-40] fe!n: fix route logic

--- Web.Client/src/app/app.config.ts
+++ Web.Client/src/app/app.config.ts
@@ -1,12 +1,37 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations'; // Updated import for simplicity
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { JwtService } from './core/auth/services/jwt.service';
+import { UserService } from './core/auth/services/user.service';
+import { apiInterceptor } from './core/interceptors/api.interceptor';
+import { tokenInterceptor } from './core/interceptors/token.interceptor';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { EMPTY } from 'rxjs';
+
+export function initAuth(jwtService: JwtService, userService: UserService) {
+  return () => (jwtService.getToken() ? userService.getCurrentUser() : EMPTY);
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideAnimationsAsync(),
+    provideZoneChangeDetection({ eventCoalescing: true, runCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(
+      withInterceptors([apiInterceptor, tokenInterceptor, errorInterceptor])
+    ),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initAuth,
+      deps: [JwtService, UserService],
+      multi: true,
+    },
   ],
 };
--- Web.Client/src/app/app.routes.ts
+++ Web.Client/src/app/app.routes.ts
@@ -1,15 +1,40 @@
-import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { UserService } from './core/auth/services/user.service';
 import { SignInComponent } from './features/sign-in/sign-in.component';
 import { SignUpComponent } from './features/sign-up/sign-up.component';
 import { ProfileSetupComponent } from './features/profile-setup/profile-setup.component';
+import { HomeComponent } from './features/home/pages/home.component';
+import { HomeGuard } from './core/guards/home.guard';
+import { ProfileSetupGuard } from './core/guards/profile-setup.guard';
+import { map } from 'rxjs/operators';
+
+const isSignUpCompleteGuard = () =>
+  inject(UserService).isSignUpComplete.pipe(
+    map((isComplete) => isComplete || inject(Router).navigate(['/sign-up']))
+  );
 
 export const routes: Routes = [
-  { path: '', component: SignInComponent },
-  { path: 'sign-up', component: SignUpComponent },
-  { path: 'sign-in', component: SignInComponent },
-  { path: 'profile-setup', component: ProfileSetupComponent },
-
-  //catch invalid routes -> redirect to home
-  { path: '**', redirectTo: '' }
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [HomeGuard],
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent,
+  },
+  {
+    path: 'sign-in',
+    component: SignInComponent,
+  },
+  {
+    path: 'profile-setup',
+    component: ProfileSetupComponent,
+    canActivate: [ProfileSetupGuard],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
