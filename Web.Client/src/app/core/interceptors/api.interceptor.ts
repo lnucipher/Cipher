d8cd3369ed16a742ee173985e19516887fdc8d7a
wovkun <volpastern@gmail.com>
2024-10-18 02:51:59 +0300

    [CP-40] fe!n: auth interceptor link added

--- Web.Client/src/app/core/interceptors/api.interceptor.ts
+++ Web.Client/src/app/core/interceptors/api.interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpInterceptorFn } from "@angular/common/http";
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
-  const apiReq = req.clone({ url: `${req.url}` });
+  const apiReq = req.clone({ url: `http://localhost:4000/${req.url}` });
   return next(apiReq);
 };
