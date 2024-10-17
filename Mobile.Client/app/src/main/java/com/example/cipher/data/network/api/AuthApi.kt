ac21ce1a3f32a091735ee0c76f093d8879e7fd26
Maksym <max.popaylo@gmail.com>
2024-10-17 16:50:16 +0300

    [CP-41] fix (jwt token handling & Fix Hilt dependencies for connection to Identity.Server

---
+++ Mobile.Client/app/src/main/java/com/example/cipher/data/network/api/AuthApi.kt
@@ -0,0 +1,32 @@
+package com.example.cipher.data.network.api
+
+import com.example.cipher.data.network.api.dto.AuthResponseDto
+import com.example.cipher.domain.models.auth.SignInRequest
+import com.example.cipher.domain.models.auth.SignUpRequest
+import com.skydoves.sandwich.ApiResponse
+import okhttp3.MultipartBody
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface AuthApi {
+
+    @Multipart
+    @POST("auth/signUp")
+    suspend fun signUp(
+        @Part("request") request: SignUpRequest,
+        @Part avatarImg: MultipartBody.Part?
+    ): ApiResponse<Unit>
+
+    @POST("auth/signIn")
+    suspend fun signIn(
+        @Body request: SignInRequest
+    ): ApiResponse<AuthResponseDto>
+
+    @GET("auth/checkIfExists")
+    suspend fun checkIfExists(
+        @Part username: String
+    ): ApiResponse<Boolean>
+}
