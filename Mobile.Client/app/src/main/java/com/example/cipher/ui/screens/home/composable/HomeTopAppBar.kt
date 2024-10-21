93f3c442e7818b8e8f3a39537b5ea70c499d375a
Maksym <max.popaylo@gmail.com>
2024-10-21 20:32:27 +0300

    [CP-41] fe!n (jwt token handling & IdentityServer connection) Add Message bubble for ChatScreen

--- Mobile.Client/app/src/main/java/com/example/cipher/ui/screens/home/composable/HomeTopAppBar.kt
+++ Mobile.Client/app/src/main/java/com/example/cipher/ui/screens/home/composable/HomeTopAppBar.kt
@@ -1,7 +1,6 @@
 package com.example.cipher.ui.screens.home.composable
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
