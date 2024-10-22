b57f90f79b3881e523d24ebcac36d2e091b44ce1
Maksym <max.popaylo@gmail.com>
2024-10-22 14:02:20 +0300

    [CP-41] fe!n (jwt token handling & IdentityServer connection) Add type-safe navigation for PersonalChat

--- Mobile.Client/app/src/main/java/com/example/cipher/domain/serializers/TimestampSerializer.kt
+++ Mobile.Client/app/src/main/java/com/example/cipher/domain/serializers/TimestampSerializer.kt
@@ -3,18 +3,19 @@ package com.example.cipher.domain.serializers
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.sql.Timestamp
 
 object TimestampSerializer : KSerializer<Timestamp> {
-    override val descriptor = PrimitiveSerialDescriptor("Timestamp", PrimitiveKind.STRING)
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Timestamp", PrimitiveKind.LONG)
 
     override fun serialize(encoder: Encoder, value: Timestamp) {
-        encoder.encodeString(value.toInstant().toString())
+        encoder.encodeLong(value.time)
     }
 
     override fun deserialize(decoder: Decoder): Timestamp {
-        return Timestamp.valueOf(decoder.decodeString())
+        return Timestamp(decoder.decodeLong())
     }
 }
