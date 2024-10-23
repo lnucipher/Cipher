a1717fa2afc22506c7581b9674ae2c2e84b17d8f
Leviathan856 <andrijtesla54@gmail.com>
2024-10-24 00:50:56 +0300

    [CP-51] hotfix (Identity)

--- Identity.Service/src/Identity.Api/Handlers.h
+++ Identity.Service/src/Identity.Api/Handlers.h
@@ -8,15 +8,15 @@ void signUpHandler(const drogon::HttpRequestPtr &request, Callback &&callback);
 void signInHandler(const drogon::HttpRequestPtr &request, Callback &&callback);
 void usernameCheck(const drogon::HttpRequestPtr &request, Callback &&callback, std::string &&username);
 
-class AccessFilter : public drogon::HttpFilter<AccessFilter>
-{
-public:
-    void doFilter(const drogon::HttpRequestPtr &req,
-                  drogon::FilterCallback &&fcb,
-                  drogon::FilterChainCallback &&fccb) override
-    {
-        auto resp = drogon::HttpResponse::newHttpResponse();
-        resp->removeHeader("Access-Control-Allow-Origin");
-        fccb();
-    }
-};
+// class AccessFilter : public drogon::HttpFilter<AccessFilter>
+// {
+// public:
+//     void doFilter(const drogon::HttpRequestPtr &req,
+//                   drogon::FilterCallback &&fcb,
+//                   drogon::FilterChainCallback &&fccb) override
+//     {
+//         auto resp = drogon::HttpResponse::newHttpResponse();
+//         resp->removeHeader("Access-Control-Allow-Origin");
+//         fccb();
+//     }
+// };
