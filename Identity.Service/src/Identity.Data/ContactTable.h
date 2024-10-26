ee776c672bde619031e59fb7f8e1c468f8cd6ccd
Leviathan856 <andrijtesla54@gmail.com>
2024-10-26 23:00:17 +0300

    [CP-67] feat (Identity): implement Contact table functionality

--- Identity.Service/src/Identity.Data/ContactTable.h
+++ Identity.Service/src/Identity.Data/ContactTable.h
@@ -1,3 +1,8 @@
+#pragma once
+#include <drogon/drogon.h>
+
+// using ContactList = std::vector<std::string>;
+
 class ContactTable
 {
 private:
@@ -9,4 +14,16 @@ private:
 
 public:
     static void createContactTable();
+    static std::shared_ptr<Json::Value> getLastContactsForUser(const std::string &userId,
+                                                               const unsigned int contactAmount = 10,
+                                                               const unsigned int startAt = 0);
+    static std::shared_ptr<Json::Value> addNewContact(const std::string &primaryUser,
+                                                      const std::string &secondaryUser);
+    static const std::shared_ptr<std::string> getIdByContact(const std::string &userId1,
+                                                             const std::string &userId2);
+    static const std::shared_ptr<Json::Value> getContactById(const std::string &contactId);
+    static const std::shared_ptr<std::string> updateLastInteract(const std::string &contactId);
+    static const std::shared_ptr<std::string> updateLastInteract(const std::string &primaryUser,
+                                                                 const std::string &secondaryUser);
+    static const std::shared_ptr<bool> deleteContact(const std::string &contactId);
 };
