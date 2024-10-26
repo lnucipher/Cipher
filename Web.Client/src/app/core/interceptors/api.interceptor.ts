8982782c58acbf909ec2669016e782aea2f25fa4
wovkun <volpastern@gmail.com>
2024-10-27 00:57:18 +0300

    [CP-66] wip: dockerfile rewert

--- Web.Client/src/app/core/interceptors/api.interceptor.ts
+++ Web.Client/src/app/core/interceptors/api.interceptor.ts
@@ -1,9 +1,9 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { environment } from '../enviroments/enviroment';
+import { environment } from '../../../enviroments/enviroment';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
   const apiReq = req.clone({
-    url: `${environment.apiUrl}${req.url}` // Uses environment-specific API URL
+    url: `http://192.168.88.192:${req.url}` // Uses environment-specific API URL
   });
   return next(apiReq);
 };
