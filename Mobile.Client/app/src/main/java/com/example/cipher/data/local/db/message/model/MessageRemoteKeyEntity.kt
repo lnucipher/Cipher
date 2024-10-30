b1be0398c92abf0730820bdc1171674a870c4c4c
Maksym <max.popaylo@gmail.com>
2024-10-30 02:22:09 +0200

    [CP-65] refactor (implement chat history pagination and caching) Approve message history loading

--- Mobile.Client/app/src/main/java/com/example/cipher/data/local/db/message/model/MessageRemoteKeyEntity.kt
+++ Mobile.Client/app/src/main/java/com/example/cipher/data/local/db/message/model/MessageRemoteKeyEntity.kt
@@ -3,7 +3,7 @@ package com.example.cipher.data.local.db.message.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity("message_remote_key")
+@Entity("message_remote_keys")
 class MessageRemoteKeyEntity (
     @PrimaryKey val id: String,
     val hasPreviousPage: Boolean,
