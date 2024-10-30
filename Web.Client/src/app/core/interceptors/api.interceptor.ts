4ccb60a06697925ab9324f6c4976a67904143bd1
wovkun <volpastern@gmail.com>
2024-10-30 23:25:39 +0200

    [CP-70] fix: production server adress fix

--- Web.Client/src/app/core/interceptors/api.interceptor.ts
+++ Web.Client/src/app/core/interceptors/api.interceptor.ts
@@ -3,7 +3,7 @@ import { environment } from '../../../environments/environment';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
   const apiReq = req.clone({
-    url: `http://192.168.88.192:${req.url}` // Uses environment-specific API URL
+    url: `http://212.23.203.37:${req.url}` // Uses environment-specific API URL
   });
   return next(apiReq);
 };
