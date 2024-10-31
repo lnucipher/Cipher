4bf31df929b6913644b04c3b6d715315f11a5bf3
Maksym <max.popaylo@gmail.com>
2024-11-01 01:02:52 +0200

    [CP-65] fix (implement chat history pagination and caching) Fix response dto for isUserExist endpoint

--- Mobile.Client/app/src/main/java/com/example/cipher/data/di/LocalModule.kt
+++ Mobile.Client/app/src/main/java/com/example/cipher/data/di/LocalModule.kt
@@ -1,25 +1,8 @@
 package com.example.cipher.data.di
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.core.DataStoreFactory
-import androidx.datastore.core.Serializer
-import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
-import androidx.datastore.dataStoreFile
-import androidx.datastore.preferences.core.PreferenceDataStoreFactory
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.emptyPreferences
-import androidx.datastore.preferences.preferencesDataStoreFile
 import androidx.room.Room
-import com.example.cipher.LocalUserProto
-import com.example.cipher.data.StorageKeys.JWT_TOKEN_PREFERENCES
-import com.example.cipher.data.StorageKeys.LOCAL_USER_PROTO_FILE
 import com.example.cipher.data.local.db.AppDatabase
-import com.example.cipher.data.local.storage.JwtTokenStorage
-import com.example.cipher.data.local.storage.LocalUserStorage
-import com.example.cipher.data.local.storage.models.LocalUserSerializer
-import com.example.cipher.domain.repository.auth.JwtTokenManager
-import com.example.cipher.domain.repository.user.LocalUserManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
