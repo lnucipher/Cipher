12cefd859539e783f339821d4185fa20e5f608dc
Maksym <max.popaylo@gmail.com>
2024-11-01 17:17:06 +0200

    [CP-65] fix (implement chat history pagination and caching) Fix Ui for devices with system navigation bars

--- Mobile.Client/app/src/main/java/com/example/cipher/ui/MainActivity.kt
+++ Mobile.Client/app/src/main/java/com/example/cipher/ui/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.cipher.ui.common.navigation.GlobalNavGraph
@@ -17,6 +20,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         enableEdgeToEdge()
+
+        actionBar?.hide()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).let { controller ->
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            controller.hide(WindowInsetsCompat.Type.navigationBars())
+        }
+
         setContent {
             CipherTheme (darkTheme = true) {
                 val navController: NavHostController = rememberNavController()
