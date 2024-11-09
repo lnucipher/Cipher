e4266643f68f25acd406b1e854859e8407868b73
Nazar Nyrka <nazarnyrka00@gmail.com>
2024-11-10 01:50:47 +0200

    bugfix: fixed hub jwt validation

--- Chat.Service/Chat.Api/DependencyInjection.cs
+++ Chat.Service/Chat.Api/DependencyInjection.cs
@@ -1,6 +1,10 @@
 ﻿using System.Text;
 using Carter;
+using Chat.Api.DelegatingHandlers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Chat.Api;
@@ -10,11 +14,34 @@ public static class DependencyInjection
     public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddCarter();
-        services.AddControllers();
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
+        services.AddHttpContextAccessor();
+
         AddDefaultCorsPolicy(services);
         ConfigureJwtAuthentication(services, configuration);
+
+        services.AddTransient<JwtAuthorizationHandler>();
+        services.AddTransient<LoggingHandler>();
+        
+        services.AddHttpClient("Identity.Service",
+                client =>
+                {
+                    client.BaseAddress = new Uri(configuration.GetConnectionString("IdentityConnection") ??
+                                                 throw new ArgumentException("IdentityService URL is not found."));
+                })
+            .AddHttpMessageHandler<JwtAuthorizationHandler>()
+            .AddHttpMessageHandler<LoggingHandler>();;
+
+
+        services.AddHttpLogging(options =>
+        {
+            options.RequestHeaders.Add("Authorization");
+            options.RequestBodyLogLimit = 4096;
+            options.ResponseBodyLogLimit = 4096;
+            options.LoggingFields = HttpLoggingFields.RequestHeaders | HttpLoggingFields.ResponseHeaders | HttpLoggingFields.RequestBody | HttpLoggingFields.ResponseBody;
+        });
+        services.Configure<JsonOptions>(options => { options.SerializerOptions.PropertyNamingPolicy = null; });
 
         return services;
     }
@@ -36,26 +63,46 @@ public static class DependencyInjection
     {
         var issuer = configuration["Jwt:Issuer"];
         var key = GetJwtSecretKey(configuration);
-
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
             .AddJwtBearer(options =>
             {
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuer = true,
-                    ValidIssuer = issuer,
-
+                    ValidateAudience = false,
                     ValidateLifetime = true,
-                    
                     ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
-                    
-                    RequireSignedTokens = true,
-                    ValidAlgorithms = new[] { SecurityAlgorithms.HmacSha512 }
+                    ValidIssuer = issuer,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key) ??
+                                                               throw new ArgumentException("JWT Secret Key is not found."))
+                };
+        
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) &&
+                            (path.StartsWithSegments("/api")))
+                        {
+                            context.Token = accessToken;
+                        }
+                
+                        return Task.CompletedTask;
+                    }
                 };
             });
+        
+        services.AddAuthorization();
     }
-    
+
     private static string GetJwtSecretKey(IConfiguration configuration)
     {
         var secretKeyPath = configuration["Jwt:Key"];
