e9a6fc16fb2ffc73e17fab0ae81d90d641d3072e
Maksym <max.popaylo@gmail.com>
2024-11-15 01:36:34 +0200

    [CP-63] refactor: Add auto scrolling for contact list when user on top

--- Mobile.Client/app/src/main/java/com/example/cipher/ui/screens/home/settings/SettingsScreen.kt
+++ Mobile.Client/app/src/main/java/com/example/cipher/ui/screens/home/settings/SettingsScreen.kt
@@ -3,15 +3,21 @@ package com.example.cipher.ui.screens.home.settings
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.cipher.ui.common.theme.CipherTheme.colors
 import com.example.cipher.ui.common.theme.CipherTheme.typography
 
 @Composable
-fun SettingsScreen() {
+fun SettingsScreen(
+    viewModel: SettingsViewModel = hiltViewModel()
+) {
+    val context = LocalContext.current
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -19,5 +25,8 @@ fun SettingsScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Settings", style = typography.heading, color = colors.primaryText)
+        Button(onClick = { viewModel.logout(context) }) {
+            Text(text = "Logout", style = typography.heading, color = colors.primaryText)
+        }
     }
 }
