b5b8ad0c72545061509d5a1c68771f08d5253c5d
Maksym <max.popaylo@gmail.com>
2024-11-15 12:08:25 +0200

    [CP-63] refactor: Fix date format in DatePicker

--- Mobile.Client/app/src/main/java/com/example/cipher/ui/common/utils/DatePickerUtil.kt
+++ Mobile.Client/app/src/main/java/com/example/cipher/ui/common/utils/DatePickerUtil.kt
@@ -1,5 +1,6 @@
 package com.example.cipher.ui.common.utils
 
+import android.annotation.SuppressLint
 import android.content.Context
 import com.example.cipher.R
 import com.example.cipher.ui.common.theme.CipherTheme
@@ -9,6 +10,7 @@ import android.app.DatePickerDialog
 class DatePickerUtil {
 
     companion object {
+        @SuppressLint("DefaultLocale")
         fun showDatePickerDialog(context: Context, onDateSelected: (String) -> Unit) {
             val calendar = Calendar.getInstance()
             val year = calendar.get(Calendar.YEAR)
@@ -18,7 +20,7 @@ class DatePickerUtil {
             val themeId = if (CipherTheme.darkTheme) R.style.DarkDatePickerTheme else R.style.LightDatePickerTheme
 
             DatePickerDialog(context, themeId, { _, selectedYear, selectedMonth, selectedDay ->
-                val formattedDate = "${selectedMonth + 1}-$selectedDay-$selectedYear"
+                val formattedDate = String.format("%02d-%02d-%d", selectedMonth + 1, selectedDay, selectedYear)
                 onDateSelected(formattedDate)
             }, year, month, day).show()
         }
