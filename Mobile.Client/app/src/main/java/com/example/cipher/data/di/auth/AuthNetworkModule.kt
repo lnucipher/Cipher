60dd2acdffa48d9f239457e4868550b4a8433919
Maksym <max.popaylo@gmail.com>
2024-11-22 17:28:06 +0200

    [CP-90] chore: Optimize AuthRepository code

--- Mobile.Client/app/src/main/java/com/example/cipher/data/di/auth/AuthNetworkModule.kt
+++ Mobile.Client/app/src/main/java/com/example/cipher/data/di/auth/AuthNetworkModule.kt
@@ -28,8 +28,8 @@ class AuthNetworkModule {
 
     @Provides
     @Singleton
-    fun provideAuthRepository(api: AuthApi, tokenManager: JwtTokenManager, localUserManager: LocalUserManager, @ApplicationContext context: Context): AuthRepository {
-        return AuthRepositoryImpl(api = api, tokenManager = tokenManager, localUserManager = localUserManager, context)
+    fun provideAuthRepository(api: AuthApi, tokenManager: JwtTokenManager, localUserManager: LocalUserManager, @ApplicationContext context: Context, moshi: Moshi): AuthRepository {
+        return AuthRepositoryImpl(api = api, tokenManager = tokenManager, localUserManager = localUserManager, context, moshi)
     }
 
     @Provides
