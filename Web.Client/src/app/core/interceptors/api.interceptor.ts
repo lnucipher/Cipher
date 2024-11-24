c61177a996fdd2239832bd19d090cc0dd9fdd19b
Nazar Nyrka <nazarnyrka00@gmail.com>
2024-11-24 18:10:01 +0200

    temp removal of https

--- Web.Client/src/app/core/interceptors/api.interceptor.ts
+++ Web.Client/src/app/core/interceptors/api.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
   const apiReq = req.clone({
-    url: `http://212.23.203.37:5001/${req.url}`,
+    url: `http://212.23.203.37:5000/${req.url}`,
   });
   return next(apiReq);
 };
