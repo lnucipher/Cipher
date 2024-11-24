5458c763777817ce6e3ea2734fb98e9e7a7848e8
Nazar Nyrka <nazarnyrka00@gmail.com>
2024-11-24 20:31:24 +0200

    bugfix: fixed ssl errors

--- Gateway.Api/Program.cs
+++ Gateway.Api/Program.cs
@@ -1,17 +1,8 @@
-using System.Net.Security;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 var config = builder.Configuration.GetSection("ReverseProxy");
 builder.Services.AddReverseProxy()
-    .ConfigureHttpClient((serviceProvider, proxyClientConfig) =>
-    {
-        proxyClientConfig.SslOptions = new SslClientAuthenticationOptions
-        {
-            RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
-        };
-    })
     .LoadFromConfig(config);
 
 var app = builder.Build();
