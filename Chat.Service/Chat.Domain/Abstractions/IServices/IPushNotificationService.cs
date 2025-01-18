91af8d18120fd38ec982f6d1ce77ab3fa52560ed
Nazar Nyrka <nazarnyrka00@gmail.com>
2025-01-18 16:56:58 +0200

    feat: send push notification to receiver on message sent

---
+++ Chat.Service/Chat.Domain/Abstractions/IServices/IPushNotificationService.cs
@@ -0,0 +1,8 @@
+﻿using Chat.Domain.Entities;
+
+namespace Chat.Domain.Abstractions.IServices;
+
+public interface IPushNotificationService
+{
+    public Task SendPushNotificationToReceiverAsync(Message sentMessage);
+}
